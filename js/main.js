740075b2139d382e8dc38529141f2c02d44d5ca8
Yosip92 <yosip92@gmail.com>
2017-05-20 11:24:07 +0300

    cut some menu items in mobile version. added menu to menu page

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function menuFunction() {
+    var navigation = '<ul style="background: green; font-size: 24px; text-decoration: none;"><li>Піца</li><li>Гарячі страви</li><li>Салати</li><li>Паста</li><li>Млинці</li><li>Напої</li><li>Сніданки</li></ul>';
+    $('#navigation').html(navigation);
+}
