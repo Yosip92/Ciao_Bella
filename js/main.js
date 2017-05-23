9013502558a7a532b07b3bdce931d2d11e831a4c
Yosip92 <yosip92@gmail.com>
2017-05-24 00:13:32 +0300

    added menu item

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function menuFunction() {
     $('#menu-dishes').slideToggle(200);
     return false;
 };
-function menuFunction1() {
-    $('.menu-drinks').slideToggle(200);
-    return false;
-};
+// function menuFunction1() {
+//     $('.menu-drinks').slideToggle(200);
+//     return false;
+// };
