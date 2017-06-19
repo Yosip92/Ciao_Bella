c3402117a96d70054df154236e0d249777f80123
Dmitro Raday <yosip92@gmail.com>
2017-06-19 19:11:08 +0300

    removed comments from js

--- js/main.js
+++ js/main.js
@@ -8,23 +8,4 @@ $(document).ready(function() {
         $('.basket').html(+$('.basket').html()+1);
         $('.basket').css('padding', '6px');
     });
-    // var todos = $('#todos').html();
-    // sessionStorage.setItem('todos', todos123);
 });
-
-// function salads() {
-//     var salad;
-//     salad = $('.salad');
-//     $('.pizza').css('display','none');
-//     $('.item-holder').html(salad);
-// }
-// function pizzas() {
-//     var pizza;
-//     pizza = $('.pizza');
-//     $('.pizza').css('display','block');
-//     $('.item-holder').html(pizza);
-// }
-// function salads() {
-//     $('.salad').slideToggle(0);
-//     return false;
-// };
