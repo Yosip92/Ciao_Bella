d210f67e51a946d51bdd0486fccd987eb6dd427d
Dmitro Raday <yosip92@gmail.com>
2017-06-21 19:40:31 +0300

    tryed to make info to sessionStorage

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ function menuFunction() {
 var session = {
   'screens': $('h2#fiesta').html(),
   'state': $('p#fiesta2').html(),
-  'sasa': $('input#fiesta3').html()
+  'sasa': $('input#fiesta3.value').html()
 };
 sessionStorage.setItem('session', JSON.stringify(session));
 var restoredSession = JSON.parse(sessionStorage.getItem('session'));
+var id0001 = '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="item1 salad"><img src="../gallery_img/pasta.png" alt=""><h2 id="fiesta">Фієста</h2><p id="fiesta2">Сир твердий, свіжі печериці, перець болгарський, куряче філе, цибуля, картопля, майонез, оцет бальзамічний</p><form><input id="fiesta3" type="radio" name="size" value="only" checked><span>250</span>гр<div class="button-buy"><input id="fiesta4" type="button" class="buy" value="Замовити"></div></form></div></div>';
 
 $(document).ready(function() {
     $('.buy').click(function() {
         $('.basket').html(+$('.basket').html()+1);
         $('.basket').css('padding', '6px');
-        $('.show').html(restoredSession);        
+        $('.show').html(id0001);
     });
 });
 
